7679a5f3f92a766ac5f965152579eb3c4e65fb43
RoboMarchello <faccclol123@gmail.com>
2024-05-26 10:06:08 +0300

    generalized into JointGroup + JointChain inherits it

--- src/scripts/objects.py
+++ src/scripts/objects.py
@@ -43,46 +43,56 @@ class Joint:
         self.position -= diff_norm * stretch
 
 
-class JointChain:
-    def __init__(self, position, joint_num, joint_length):
-        self.position = position
-        self.target_point = Point((0, 0))
+class JointGroup:
+    def __init__(self, origin, target_point:Point, joints):
+        self.origin = origin
 
-        # generate joints
-        self.joints = []
-        
-        for i in range(joint_num):
-            if i == 0:
-                joint = Joint(self.target_point, joint_length)
-            else:
-                joint = Joint(self.joints[i - 1], joint_length)
-            self.joints.append(joint)
+        self.target_point = target_point
+        self.joints = joints
+
+    def draw(self, surface):
+        pass
 
     def update(self):
         for joint in self.joints:
             joint.follow_point()
         
-        displacement = self.position - self.joints[-1].position
+        displacement = self.origin - self.joints[-1].position
         for joint in self.joints:
             joint.position += displacement
-
         self.target_point.position += displacement
 
     def get_points(self):
         points = []
         
         points.append(self.target_point.position)
-
         for joint in self.joints:
             points.append(joint.position)
 
         return points
+
+    def set_target(self, position):
+        self.target_point.update(position)
+
+
+class JointChain(JointGroup):
+    def __init__(self, origin, joint_num, joint_length):
+        target_point = Point((0, 0))
+
+        # generate joints
+        joints = []
+        
+        for i in range(joint_num):
+            if i == 0:
+                joint = Joint(target_point, joint_length)
+            else:
+                joint = Joint(joints[i - 1], joint_length)
+            joints.append(joint)
+
+        super().__init__(origin, target_point, joints)
 
     def draw(self, surface):
         for joint in self.joints:
             joint.draw(surface)
 
         self.target_point.draw(surface)
-
-    def set_target(self, position):
-        self.target_point.update(position)
